dc614df0efaf1ee8512032d463e94cbd3fb88b0b
Bui-Van-Dat <89328299+Bui-Van-Dat@users.noreply.github.com>
2023-10-19 23:27:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     introTitle: 'Hí em, lại là Đạt đây!',
-    introDesc: `Nhân ngày 20/10, xin lời chúc tình cảm nhất. Chúc các chị em luôn xinh đẹp, vui vẻ và tràn đầy năng lượng. Mong rằng tình yêu và sự quan tâm sẽ luôn được đổ đầy trong cuộc sống của mỗi người. Hãy tiếp tục phấn đấu và theo đuổi ước mơ của mình. Chúc mừng ngày của các chị em!`,
+    introDesc: `Nhân ngày 20/10, xin gửi lời chúc tình cảm nhất. Chúc các chị em luôn xinh đẹp, vui vẻ và tràn đầy năng lượng. Mong rằng tình yêu và sự quan tâm sẽ luôn được đổ đầy trong cuộc sống của mỗi người. Hãy tiếp tục phấn đấu và theo đuổi ước mơ của mình. Chúc mừng ngày của các chị em!`,
     btnIntro: 'next',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
@@ -8,7 +8,7 @@ const CONFIG = {
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người, có người vẫn đang viết những lời chúc đến bạn hãy viết gì đó cho cậu ấy đi nè <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Anh dễ thương lại đẹp trai nữa <33333333',
+    reply: 'Anh Đạt đã dễ thương lại đẹp trai nữa <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Còn chờ gì nữa mà không inbox cho a đi nào kk.',
     btnAccept: 'Okiiiii lun <3',
