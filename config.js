ecff740b3f1a7da0f989c13cc1f1981ddab4a1b0
Bui-Van-Dat <89328299+Bui-Van-Dat@users.noreply.github.com>
2023-10-19 23:23:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Chào cậu, lại là Hiệu đây!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi. 
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    introTitle: 'Hí em, lại là Đạt đây!',
+    introDesc: `Nhân ngày 20/10, xin lời chúc tình cảm nhất. Chúc các chị em luôn xinh đẹp, vui vẻ và tràn đầy năng lượng. Mong rằng tình yêu và sự quan tâm sẽ luôn được đổ đầy trong cuộc sống của mỗi người. Hãy tiếp tục phấn đấu và theo đuổi ước mơ của mình. Chúc mừng ngày của các chị em!`,
+    btnIntro: 'next',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Trên thế giới hơn 7 tỉ người, có người vẫn đang viết những lời chúc đến bạn hãy viết gì đó cho cậu ấy đi nè <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Anh dễ thương lại đẹp trai nữa <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Còn chờ gì nữa mà không inbox cho a đi nào kk.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://https://www.facebook.com/hieu.duongduc.12' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/dat.rongvang.73' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
